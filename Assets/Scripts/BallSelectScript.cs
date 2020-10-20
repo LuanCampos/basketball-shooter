38a9cf1add582e11394ee8478d542bd0c57556b5
Luan Campos <campos.luan@outlook.com>
2020-10-20 15:56:00 -0300

    Add select ball script

---
+++ Assets/Scripts/BallSelectScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallSelectScript : MonoBehaviour
+{
+    private List<Button> buttons = new List<Button>();
+	
+	void Awake()
+	{
+		GetButtonsAndListeners();
+	}
+	
+	void GetButtonsAndListeners()
+	{
+		GameObject[] btns = GameObject.FindGameObjectsWithTag("MenuBall");
+		for (int i = 0; i < btns.Length; i++)
+		{
+			buttons.Add(btns[i].GetComponent<Button>());
+			buttons[i].onClick.AddListener(() => SelectBall());
+		}
+	}
+	
+	public void SelectBall()
+	{
+		int index = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+		Debug.Log("The Selected index is " + index);
+		// inform game manager which index is select
+	}		
+	
+}
