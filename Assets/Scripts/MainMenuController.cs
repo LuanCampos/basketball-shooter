b8ba61b5a9954d0acc233d210485b0849f754159
Luan Campos <campos.luan@outlook.com>
2020-10-20 11:49:07 -0300

    Add Main Menu controller

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+	private Animator mainAnim;
+	private Animator ballAnim;
+	
+	void Awake()
+	{
+		mainAnim = GameObject.Find("Main Holder").GetComponent<Animator>();
+		ballAnim = GameObject.Find("Balls Holder").GetComponent<Animator>();
+	}
+	
+	public void PlayGame()
+	{
+		Application.LoadLevel("Gameplay");
+	}
+	
+	public void SelectBall()
+	{
+		mainAnim.Play("FadeOut");
+		ballAnim.Play("FadeIn");
+	}
+	
+	public void BackToMenu()
+	{
+		ballAnim.Play("FadeOut");
+		mainAnim.Play("FadeIn");
+	}
+
+}
